ce743541efda98c9b3f6899b9652cc1c49864ef7
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2021-08-06 13:52:54 +0100

    ADD player choose pool

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+// RUCH KOMPUTERA
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('Ruch komputera: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
@@ -12,3 +14,20 @@ if(randomNumber == 1) {
     computerMove = 'nożyce';
 
 printMessage('Mój ruch to: ' + computerMove);
+
+// RUCH GRACZA
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput == '2') {
+    playerMove = 'papier;'
+} else(playerInput == '3')
+    playerMove = 'nożyce';
+
+printMessage('Twój ruch to: ' + playerMove);
