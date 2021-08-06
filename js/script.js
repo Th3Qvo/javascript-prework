2c88e1bb11fd2f2986c8db16d90489752683f595
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2021-08-06 13:38:38 +0100

    ADD computer player drowing pool

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove = 'kamień';
-let playerMove = "papier";
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1) {
+  computerMove = 'kamień';
+} else if(randomNumber == 2) {
+    computerMove = 'papier';
+} else(randomNumber == 3)
+    computerMove = 'nożyce';
+
+printMessage('Mój ruch to: ' + computerMove);
